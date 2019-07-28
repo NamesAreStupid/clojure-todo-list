ddf9a2bfdc9fa3511845523fe85e1894d78afa46
Kevin H <kevin.harbarth@gmx.net>
2019-07-28 19:31:04 +0200

    Added basic ring server functionality

--- src/clojure_todo_list/core.clj
+++ src/clojure_todo_list/core.clj
@@ -1,6 +1,29 @@
-(ns clojure-todo-list.core)
+(ns clojure-todo-list.core
+  (:require [ring.adapter.jetty :as jetty]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; (defn foo
+;;   "I don't do a whole lot."
+;;   [x]
+;;   (println x "Hello, World!"))
+
+;; (foo " Yo, ")
+
+(defn welcome
+  "A ring handler to process all requests sent to the webapp"
+  [request]
+  (if (= "/" (:uri request))
+    {:status 200
+     :body "<h1>Hello, Clojure World</h1>
+            <p>Welcome to your first Clojure app.</p>"
+     :headers {}}
+    {:status 404
+     :body "<h1>This is not the page you are looking for</h1>
+            <p>Sorry, the page you requested was not found!></p>"
+     :headers {}}))
+
+(defn -main
+  [port-number]
+  (jetty/run-jetty welcome
+     {:port (Integer. port-number)}))
+
+(def port-number 8000)
